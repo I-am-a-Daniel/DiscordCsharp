25fbe12640bede3b3f5c1a37f576e41cf3e71f12
I-am-a-Daniel <86127739+I-am-a-Daniel@users.noreply.github.com>
2024-01-14 00:23:31 +0100

    Implemented Forecasts for /wr command
    Implemented Timestamp -> DateTime (and String) conversion
    Implemented Wind speed description

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Discord;
@@ -53,7 +54,8 @@ class Program
         var weather_command = new SlashCommandBuilder()
             .WithName("wr")
             .WithDescription("Weather information")
-            .AddOption("city", ApplicationCommandOptionType.String, "City name", isRequired: true);
+            .AddOption("city", ApplicationCommandOptionType.String, "City name", isRequired: true)
+            .AddOption("forecast", ApplicationCommandOptionType.Integer, "Forecast in hours", isRequired: false);
         try
         {
             await _client.CreateGlobalApplicationCommandAsync(weather_command.Build());
@@ -72,8 +74,21 @@ class Program
             case "ping":
                 await command.RespondAsync("Pong"); break;
             case "wr":
-                //await command.RespondAsync(WeatherHandler.GetWeatherDataForCity((string)command.Data.Options.First().Value));
-                await command.RespondAsync(embed: WeatherHandler.GetWeatherDataForCity((string)command.Data.Options.First().Value).Build());
+                if(command.Data.Options.Count == 1) //Nincs forecast
+                {
+                    await command.RespondAsync(embed: WeatherHandler.GetWeatherDataForCity((string)command.Data.Options.First().Value).Build());
+                }
+                else                                //Van forecast
+                {
+                    var temp = command.Data.Options.FirstOrDefault(param => param.Name == "forecast").Value;  //FIXME: Conversion error
+                    int hours = Convert.ToInt32(temp);
+                    if (hours > 100 || hours < 0)
+                    {
+                        await command.RespondAsync("3 és 100 óra közötti időtávot adj meg.", ephemeral: true);
+                    }
+                    else
+                        await command.RespondAsync(embed: WeatherHandler.GetWeatherForecastForCity((string)command.Data.Options.First().Value, hours).Build());
+                }
                 break;
             default:
                 await command.RespondAsync("Valami nem jó"); break;
@@ -85,23 +100,38 @@ class Program
         return Task.CompletedTask;
     }
 }
-
+public class Utilities
+{
+    public static string Timestamp2String(double timestamp)                 //TODO: Untested
+    {
+        System.DateTime dateTime = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+        dateTime = dateTime.AddSeconds(timestamp);
+        string? str = dateTime.ToString("yyyy. MMMM dd. HH:mm") ?? "Ismeretlen időpont";
+        return str;
+    }
+}
 public class Weather
 {
     private static string apikey = "";
-    public static string GetJson(string city)
+    public static string GetCurrentWeatherJson(string city)
     {
             string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}&lang=hu";
             var json = new WebClient().DownloadString(url);
             return json;
     }
+    public static string GetForecastJson(string city)
+    {
+        string url = $"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={apikey}&lang=hu";
+        var json = new WebClient().DownloadString(url);
+        return json;
+    }
 }
 
 public class WeatherHandler
 {
-    public static EmbedBuilder GetWeatherDataForCity(string city)
+    public static EmbedBuilder GetWeatherDataForCity(string city)       //TODO: Error handling, e.g. Non-existing location
     {
-        string json = Weather.GetJson(city);
+        string json = Weather.GetCurrentWeatherJson(city);
         JsonDocument jsondoc = JsonDocument.Parse(json);
         JsonElement root = jsondoc.RootElement;
         Location location = new Location();
@@ -109,7 +139,7 @@ public class WeatherHandler
         location.Humidity = root.GetProperty("main").GetProperty("humidity").GetInt32();
         location.WindSpeed = Convert.ToInt32(root.GetProperty("wind").GetProperty("speed").GetDouble() * 3.6);
         location.Name = root.GetProperty("name").ToString() ?? city;
-        location.Weather = char.ToUpper(root.GetProperty("weather")[0].GetProperty("description").ToString()[0]) + root.GetProperty("weather")[0].GetProperty("description").ToString().Substring(1); //Édes istenem
+        location.Weather = char.ToUpper(root.GetProperty("weather")[0].GetProperty("description").ToString()[0]) + root.GetProperty("weather")[0].GetProperty("description").ToString()[1..]; //Édes istenem
         location.WindDirection = GetWindDirString(root.GetProperty("wind").GetProperty("deg").GetInt32()) ?? "Változó irányú"; // Ritkán, de van olyan, hogy szélirány nincs a jsonben, szélerősség viszont igen
         location.Icon = $"https://openweathermap.org/img/wn/{root.GetProperty("weather")[0].GetProperty("icon")}@2x.png";
         //string response = $"Időjárás {location.Name} területén: {location.Weather}, {location.Temperature} °C.";
@@ -117,6 +147,27 @@ public class WeatherHandler
             .WithTitle($"Időjárás {location.Name} területén")
             .WithDescription($"{location.Weather}, {location.Temperature} °C.\n {location.WindDirection} szél, {location.WindSpeed} km/h.")
             .WithThumbnailUrl(location.Icon);
+        return response;
+    }
+
+    public static EmbedBuilder GetWeatherForecastForCity(string city, int hour)
+    {
+        string json = Weather.GetForecastJson(city);
+        JsonDocument jsondoc = JsonDocument.Parse(json);
+        JsonElement root = jsondoc.RootElement;
+        Location location = new Location();
+        int n = hour / 3;
+        location.Temperature = Convert.ToInt32(root.GetProperty("list")[n].GetProperty("main").GetProperty("temp").GetDouble() - 273);
+        location.Weather = char.ToUpper(root.GetProperty("list")[n].GetProperty("weather")[0].GetProperty("description").ToString()[0]) + root.GetProperty("list")[n].GetProperty("weather")[0].GetProperty("description").ToString()[1..]; //Milyen kár, hogy nincs String.Title()
+        location.WindForecast = GetWindSpeedText(Convert.ToInt32(root.GetProperty("list")[n].GetProperty("wind").GetProperty("speed").GetDouble() * 3.6)) ?? "Szélcsend";
+        location.Icon = $"https://openweathermap.org/img/wn/{root.GetProperty("list")[n].GetProperty("weather")[0].GetProperty("icon")}@2x.png";
+        location.Name = root.GetProperty("city").GetProperty("name").ToString();
+        string? stamp = Utilities.Timestamp2String(double.Parse(root.GetProperty("list")[n].GetProperty("dt").ToString())) ?? "Ismeretlen időpont";     //!!Ez dt és nem dt_txt mint a python botban, jó volt erre is elbaszni fél órát. - TODO kitalálni, hogy ott miért működött egyáltalán
+        var response = new EmbedBuilder()
+            .WithTitle($"Előrejelzés {location.Name} területére")
+            .WithDescription($"{location.Weather}, {location.Temperature} °C. {location.WindForecast}.")
+            .WithThumbnailUrl(location.Icon)
+            .WithFooter($"Érvényesség: {stamp} + 3 óra");
         return response;
     }
 
@@ -129,6 +180,7 @@ public class WeatherHandler
         public double WindSpeed { get; set; }
         public string? WindDirection { get; set; }
         public string? Icon { get; set; }
+        public string? WindForecast { get; set; }
     }
 
     public static string GetWindDirString(int deg)
@@ -151,6 +203,16 @@ public class WeatherHandler
         else if (deg > 292 && deg <= 337)
             direction = "Északnyugati";
         return direction;
+    }
+    public static string GetWindSpeedText(int speed)
+    {
+        string text = "";
+        if (speed < 5) text = "Szélcsend";
+        else if (speed >= 5 && speed < 15) text = "Enyhe szél";
+        else if (speed >= 15 && speed < 25) text = "Mérsékelt szél";
+        else if (speed >= 25 && speed < 35) text = "Élénk szél";
+        else if (speed >= 35) text = "Viharos szél";
+        return text;
     }
 }
 
