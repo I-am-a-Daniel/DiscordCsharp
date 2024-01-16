0c29f1c1475d9ff341905a87a37258fa6f5d7394
I-am-a-Daniel <86127739+I-am-a-Daniel@users.noreply.github.com>
2024-01-16 17:47:01 +0100

    Tempfix for English month names in forecasts

--- Utilities.cs
+++ Utilities.cs
@@ -1,10 +1,12 @@
-﻿public class Utilities
+﻿using System.Globalization;
+
+public class Utilities
 {
     public static string Timestamp2String(double timestamp)                 //TODO: Untested
     {
         System.DateTime dateTime = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
         dateTime = dateTime.AddSeconds(timestamp);
-        string? str = dateTime.ToString("yyyy. MMMM dd. HH:mm") ?? "Ismeretlen időpont";
+        string? str = dateTime.ToString("yyyy. MMMM dd. HH:mm", CultureInfo.CreateSpecificCulture("hu")) ?? "Ismeretlen időpont";   //Ha magyar nyelvű gépről fut, nem kell hardcodeolni
         return str;
     }
 }
