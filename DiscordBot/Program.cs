e9d56534f1f2b74a168d4867e86032091582db36
I-am-a-Daniel <r.daniel9596.gaming@gmail.com>
2024-03-01 17:08:58 +0100

    Revert "Begin work with translation module"
    This reverts commit f289303c0c5b73d02b266e608832bbd8aef2e8a6.

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,102 +1,32 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
-using System.Threading.Tasks;
-using Discord;
-using Discord.WebSocket;
-class Program
+﻿using System.Threading.Tasks;
+using DiscordBot.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DiscordBot
 {
-    public static DiscordSocketClient _client;
-
-    static async Task Main(string[] args)
+    class Program
     {
-        var program = new Program();
-        await program.RunBotAsync();
-    }
-
-    public async Task RunBotAsync()
-    {
-        _client = new DiscordSocketClient();
-        _client.Log += LogAsync;
-        Keys.Read();
-
-
-        await _client.LoginAsync(TokenType.Bot, Keys.main);
-        await _client.StartAsync();
-        _client.Ready += () =>
+        static async Task Main(string[] args)
         {
-            //Console.WriteLine("Bot successfully connected");
-            Client_Ready(); // Valami warningot ír rá mert nincs await, majd meglátjuk h baj-e
-            return Task.CompletedTask;
-        };
-        _client.SlashCommandExecuted    +=  CommandHandler.Execute;
-        _client.ButtonExecuted          +=  InteractionHandler.HandleButtonPress;
+            var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
         
-        await Task.Delay(-1);
-    }
+        var serviceProvider = new ServiceCollection()
+            .Configure<OWMSettings>(options => configuration.GetSection("OWM").Bind(options))
+            .Configure<DiscordSettings>(options => configuration.GetSection("Discord").Bind(options))
+                .AddSingleton<WeatherClient>()
+                .AddSingleton<DiceGameManager>()
+                .AddSingleton<WeatherHandler>()
+                .AddSingleton<CommandHandler>()
+                .AddSingleton<InteractionHandler>()
+                .AddSingleton<Container>()
+                .BuildServiceProvider();
 
-    public async Task Client_Ready()
-    {
-        Console.WriteLine("Parancsok betöltése...");
-        await CommandHandler.RegisterCommand("coldest", "Leghidegebb hőmérséklet a következő 5 napban", new SlashCommandOptionBuilder()
-            .WithName("település")
-            .WithType(ApplicationCommandOptionType.String)
-            .WithDescription("településnév")
-            .WithRequired(true));
-        await CommandHandler.RegisterCommand("dice", "Kockajáték indítása", new SlashCommandOptionBuilder()
-            .WithName("target")
-            .WithType(ApplicationCommandOptionType.Integer)
-            .WithDescription("Ennyi pontot kell elérni a nyeréshez (20-100)")
-            .WithRequired(false));
-        await CommandHandler.RegisterCommand("hottest", "Legmelegebb hőmérséklet a következő 5 napban", new SlashCommandOptionBuilder()
-            .WithName("település")
-            .WithType(ApplicationCommandOptionType.String)
-            .WithDescription("településnév")
-            .WithRequired(true));
-        await CommandHandler.RegisterCommand("nextclear", "A legkorábbi időpontot mutatja meg, mikor derűs idő várható", new SlashCommandOptionBuilder()
-            .WithName("település")
-            .WithType(ApplicationCommandOptionType.String)
-            .WithDescription("településnév")
-            .WithRequired(true));
-        await CommandHandler.RegisterCommand("nextrain", "A legkorábbi időpontot mutatja meg, mikor esős idő várható", new SlashCommandOptionBuilder()
-            .WithName("település")
-            .WithType(ApplicationCommandOptionType.String)
-            .WithDescription("településnév")
-            .WithRequired(true));
-        await CommandHandler.RegisterCommand("nextsnow", "A legkorábbi időpontot mutatja meg, mikor havazás várható", new SlashCommandOptionBuilder()
-            .WithName("település")
-            .WithType(ApplicationCommandOptionType.String)
-            .WithDescription("településnév")
-            .WithRequired(true));
-        await CommandHandler.RegisterCommand("pong", "Debug Feature");
-        await CommandHandler.RegisterCommand("tr", "Google Transzláció", new SlashCommandOptionBuilder()
-            .WithName("target")
-            .WithType(ApplicationCommandOptionType.String)
-            .WithDescription("Célnyelv")
-            .WithRequired(true),
-            new SlashCommandOptionBuilder()
-            .WithName("text")
-            .WithType(ApplicationCommandOptionType.String)
-            .WithDescription("Szöveg")
-            .WithRequired(true)
-            );
-        await CommandHandler.RegisterCommand("wr", "Időjárás lekérdezése", new SlashCommandOptionBuilder()
-            .WithName("település")
-            .WithType(ApplicationCommandOptionType.String)
-            .WithDescription("településnév")
-            .WithRequired(true),
-            new SlashCommandOptionBuilder()
-            .WithName("előrejelzés")
-            .WithType(ApplicationCommandOptionType.Integer)
-            .WithDescription("Hány óra múlva szeretnéd kérni az előrejelzést")
-            .WithRequired(false));
-        Console.WriteLine("Parancsok betöltésének vége.");
-    }
+            var container = serviceProvider.GetRequiredService<Container>();
 
-    private Task LogAsync(LogMessage log)
-    {
-        Console.WriteLine(log);
-        return Task.CompletedTask;
+            await container.RunBotAsync();
+        }
     }
 }
-
--- DiscordBot/TranslationHandler.py
+++
@@ -1,6 +0,0 @@
-﻿import pygoogletranslation
-
-def Translate(tar, txt):
-    TR = pygoogletranslation.Translator()
-    result = TR.translate(txt.lower(), dest=tar).text
-    return result
