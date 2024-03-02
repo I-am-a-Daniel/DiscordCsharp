7a29162573c8162b11cfd19e3fe7476364b498c8
I-am-a-Daniel <r.daniel9596.gaming@gmail.com>
2024-03-03 00:13:57 +0100

    Fixed encoding issue in translator module
    TODO: Fix language acquisition

--- DiscordBot/TranslationHandler.py
+++ DiscordBot/TranslationHandler.py
@@ -1,7 +1,10 @@
 ﻿# -*- coding: utf-8 -*-
 import pygoogletranslation
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+sys.stdin.reconfigure(encoding='utf-8')
 TR = pygoogletranslation.Translator()
 tar = input("")
 txt = input("")
-result = TR.translate(txt.lower(), dest=tar).text
+result = TR.translate(txt.lower(), dest="en").text
 print(result)
--- DiscordBot/TranslationHandlerCsharp.cs
+++ DiscordBot/TranslationHandlerCsharp.cs
@@ -1,9 +1,7 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Diagnostics;
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Text;
+
 
 public class TranslationHandlerCsharp
 {
@@ -11,23 +9,26 @@ public class TranslationHandlerCsharp
     {
         try
         {
+
             using var process = new Process();
             process.StartInfo.FileName = "C:\\Program Files (x86)\\Microsoft Visual Studio\\Shared\\Python39_64\\python.exe";
             process.StartInfo.Arguments = $"C:/Users/rdani/source/repos/DiscordBot/DiscordBot/bin/Debug/net6.0/TranslationHandler.py";
             process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.StandardInputEncoding = Encoding.UTF8;
             process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.StandardOutputEncoding = Encoding.UTF8;
             process.Start();
             //process.StandardInput.WriteLine($"Translate({target}, {text})");
             process.StandardInput.WriteLine($"{target}");
             process.StandardInput.WriteLine($"{text}");
             var result = "Teszt";
-
             while (!process.StandardOutput.EndOfStream)
             {
                 result = process.StandardOutput.ReadLine();
                 Console.WriteLine(result);
             }
             process.WaitForExit();
+
             return result;
 
         }
