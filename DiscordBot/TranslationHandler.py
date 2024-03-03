dc8a4a3e6746d434cdc50525020d0b0cbab7fb01
I-am-a-Daniel <r.daniel9596.gaming@gmail.com>
2024-03-03 16:35:26 +0100

    Tempfix translate
    Hátha nem veszik észre xddddddd

--- DiscordBot/TranslationHandler.py
+++ DiscordBot/TranslationHandler.py
@@ -1,10 +1,16 @@
-﻿# -*- coding: utf-8 -*-
-import pygoogletranslation
+﻿import pygoogletranslation
 import sys
 sys.stdout.reconfigure(encoding='utf-8')
 sys.stdin.reconfigure(encoding='utf-8')
 TR = pygoogletranslation.Translator()
-tar = input("")
-txt = input("")
-result = TR.translate(txt.lower(), dest="en").text
+entry = input("")
+tar = entry.split("|")[0].strip()
+txt = entry.split("|")[1]
+result = "Not yet implemented"
+if tar.find("en") != -1:
+	result = TR.translate(text = txt.lower(), dest = "en").text
+elif tar.find("hu") != -1:
+	result = TR.translate(text = txt.lower(), dest = "hu").text
+elif tar.find("de") != -1:
+	result = TR.translate(text = txt.lower(), dest = "de").text
 print(result)
